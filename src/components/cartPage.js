c5ee1ac6250717c3bdee79b6d8216fe82e4c39b4
arha0822 <arunav.harsh@netcracker.com>
2023-02-25 02:40:16 +0530

    fixes in cartPage.js

--- src/components/cartPage.js
+++ src/components/cartPage.js
@@ -11,8 +11,8 @@ export default function CartPage() {
     const cartItems = useSelector(state => state.cart.items)
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
     const dispatch = useDispatch()
-    let disabled = cartItems.length > 0 ? false: true
-    let path = isLoggedIn ? '/checkout': '/signup'
+    // let disabled = cartItems.length > 0 ? false: true
+    let path = isLoggedIn ? '/checkout' : '/signup'
 
     const removeFromCart = (item, event) => {
         dispatch(removeItem(item))
@@ -20,7 +20,7 @@ export default function CartPage() {
 
     const cartItemsJsx = (
         cartItems.map(items => {
-            return <CartItemList key={items.id} items={items} onClick={removeFromCart}/>
+            return <CartItemList key={items.id} items={items} onClick={removeFromCart} />
         })
     )
 
@@ -29,9 +29,15 @@ export default function CartPage() {
     return (
         <div>
             <Header />
-            { cartItems.length > 0 ? cartItemsJsx : <h4>Please Add Items in Cart</h4>  }
+            {cartItems.length > 0 ? cartItemsJsx : <h4>Please Add Items in Cart</h4>}
             <NavLink to={path}>
-                <Button variant='success' disabled={disabled} className='mt-2'> Checkout </Button>
+                <Button 
+                    variant='success' 
+                    disabled={cartItems.length > 0 ? false : true} 
+                    className='mt-2 mb-4'
+                > 
+                    Checkout 
+                </Button>
             </NavLink>
         </div>
     )
